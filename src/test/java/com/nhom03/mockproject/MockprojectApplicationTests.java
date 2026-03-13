a73d70f1d23dbe2ce5005dedaca3ce82abeb0ac4
vantu <tulevan526@gmail.com>
2026-03-13 09:57:32 +0700

    Initial project setup

---
+++ src/test/java/com/nhom03/mockproject/MockprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom03.mockproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
